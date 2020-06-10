53e74ed31d5039abaf90c25d0f46efd11da60f92
Hasan Ozovali <ozovalihasan@gmail.com>
2020-06-11 00:10:11 +0300

    Fix errors found by rubocop

--- bot.rb
+++ bot.rb
@@ -6,15 +6,14 @@ require 'uri'
 require 'net/http'
 
 Slack.configure do |config|
-	config.token = ENV['SLACK_BOT_TOKEN']
-	fail 'Missing API token' unless config.token
+  config.token = ENV['SLACK_BOT_TOKEN']
+  raise 'Missing API token' unless config.token
 end
 
 # Slack Ruby client
 $client = Slack::Web::Client.new
 # Plays object
 $plays = {}
-
 
 class API < Sinatra::Base
   # Send response of message to response_url
@@ -32,112 +31,100 @@ class API < Sinatra::Base
     request_data = JSON.parse(request.body.read)
 
     case request_data['type']
-      when 'url_verification'
-        # URL Verification event with challenge parameter
-        request_data['challenge']
-      when 'event_callback'
-        # Verify requests
-        if request_data['token'] != ENV['SLACK_VERIFICATION_TOKEN']
-          return
-        end
+    when 'url_verification'
+      # URL Verification event with challenge parameter
+      request_data['challenge']
+    when 'event_callback'
+      # Verify requests
+      return if request_data['token'] != ENV['SLACK_VERIFICATION_TOKEN']
 
-        team_id = request_data['team_id']
-        event_data = request_data['event']
-        case event_data['type']
-          # Message IM event
-          when 'message'
-            if event_data['subtype'] == 'bot_message' || event_data['subtype'] == 'message_changed'
-              return
-            end
-            Events.message(team_id, event_data)
-          else
-            puts "Unexpected events\n"
-        end
-        status 200
+      team_id = request_data['team_id']
+      event_data = request_data['event']
+      case event_data['type']
+        # Message IM event
+      when 'message'
+        return if event_data['subtype'] == 'bot_message' || event_data['subtype'] == 'message_changed'
+
+        Events.message(team_id, event_data)
+      else
+        puts "Unexpected events\n"
+      end
+      status 200
     end
   end
 
   post '/slack/attachments' do
-
     request.body.rewind
     request_data = request.body.read
 
     # Convert
-    request_data = URI.decode_www_form_component(request_data, enc=Encoding::UTF_8)
+    request_data = URI.decode_www_form_component(request_data, enc = Encoding::UTF_8)
 
     # Parse and remove "payload=" from beginning of string
     request_data = JSON.parse(request_data.sub!('payload=', ''))
     url = request_data['response_url']
- 
+
     case request_data['callback_id']
       # Start order callback
-      when 'play:start'
-        # Modify message to acknowledge
-        msg = request_data['original_message']
-        msg['text'] = ':video_game: OK I\'m starting the game.'
-        msg['attachments'] = []
-        API.send_response(url, msg)
-        Bot.start_game(request_data['user']['id'], request_data['original_message'], url)
+    when 'play:start'
+      # Modify message to acknowledge
+      msg = request_data['original_message']
+      msg['text'] = ':video_game: OK I\'m starting the game.'
+      msg['attachments'] = []
+      API.send_response(url, msg)
+      Bot.start_game(request_data['user']['id'], request_data['original_message'], url)
       # Select type callback
-      when 'play:select_position'
-        begin
-          user_id = request_data['user']['id']
-          msg = request_data['original_message']
-          chosen = request_data['actions'][0]['value'].to_i
-          if chosen == 0
-            Bot.start_game(user_id, request_data['original_message'], url) 
-            break
-          end
-          Bot.update_board(request_data,chosen,'X')
-          break if Bot.check_winner_draw(user_id,url,msg)
-          if $plays[user_id][:turn_number] < 9
-            msg['text'] = ' It is my turn.'
-            msg['attachments'] = Bot.board_last(user_id)
-            API.send_response(url, msg)
-          end
-          sleep(1)
-
-          chosen = Bot.choose_position(user_id)
-          msg['text'] = "I\'m chosing #{chosen}."
-          API.send_response(url, msg)
-          Bot.update_board(request_data,chosen,'O')
-          sleep(1)
-          
-          break if Bot.check_winner_draw(user_id,url,msg)
-            
-
-          Bot.start_game(user_id, request_data['original_message'], url) 
-        end while false
-      # Select option callback
-      when 'play:finish'
+    when 'play:select_position'
+      begin
+        user_id = request_data['user']['id']
         msg = request_data['original_message']
-        msg['text'] = ':ok: If you want to play more, I will be here. Only send any message to me '
-        msg['attachments'] = []
+        chosen = request_data['actions'][0]['value'].to_i
+        if chosen.zero?
+          Bot.start_game(user_id, request_data['original_message'], url)
+          break
+        end
+        Bot.update_board(request_data, chosen, 'X')
+        break if Bot.check_winner_draw(user_id, url, msg)
+
+        if $plays[user_id][:turn_number] < 9
+          msg['text'] = ' It is my turn.'
+          msg['attachments'] = Bot.board_last(user_id)
+          API.send_response(url, msg)
+        end
+        sleep(1)
+
+        chosen = Bot.choose_position(user_id)
+        msg['text'] = "I\'m chosing #{chosen}."
         API.send_response(url, msg)
-        $client.im_open(user: user_id)
-        
-      else
+        Bot.update_board(request_data, chosen, 'O')
+        sleep(1)
+
+        break if Bot.check_winner_draw(user_id, url, msg)
+
+        Bot.start_game(user_id, request_data['original_message'], url)
+      end while false
+      # Select option callback
+    when 'play:finish'
+      msg = request_data['original_message']
+      msg['text'] = ':ok: If you want to play more, I will be here. Only send any message to me '
+      msg['attachments'] = []
+      API.send_response(url, msg)
+      $client.im_open(user: user_id)
+
     end
     status 200
   end
 end
-  
-class Events
 
+class Events
   # Handle message IM event
-  def self.message(team_id, event_data)
-    if event_data['user']
-      user_id = event_data['user']
-      # Handle direct message
-      Bot.handle_direct_message(event_data)
-    end
-    # Handle direct messages
+  def self.message(_team_id, event_data)
+    # Handle direct message
+    Bot.handle_direct_message(event_data) if event_data['user']
   end
 end
 
-
 class Bot
-
   def self.intro(user_id)
     # Open IM
 
@@ -152,26 +139,28 @@ class Bot
           name: 'start',
           text: '   YES   ',
           type: 'button',
-          value: 'play:start',
+          value: 'play:start'
         }
-        ]},
-        {
-          color: '#FF0000',
-          title: '',
-          callback_id: 'play:finish',
-          actions: [
-            {
-              name: 'start',
-              text: '   NO   ',
-              type: 'button',
-              value: 'play:finish',
-            }
-            ]}]
-    if !res.channel.id.nil?
+      ]
+    },
+                   {
+                     color: '#FF0000',
+                     title: '',
+                     callback_id: 'play:finish',
+                     actions: [
+                       {
+                         name: 'start',
+                         text: '   NO   ',
+                         type: 'button',
+                         value: 'play:finish'
+                       }
+                     ]
+                   }]
+    unless res.channel.id.nil?
       # Send message
       $client.chat_postMessage(
-        channel: res.channel.id, 
-        text: "I am tic-tac-toe bot, and I\'m here to play tic-tac-toe :x: :o: ", 
+        channel: res.channel.id,
+        text: "I am tic-tac-toe bot, and I\'m here to play tic-tac-toe :x: :o: ",
         attachments: attachments.to_json
       )
     end
@@ -182,18 +171,16 @@ class Bot
     if $plays[user_id].nil?
       # Starts new game
       $plays[user_id] = {
-        :positions => [1,2,3,4,5,6,7,8,9],:turn_number => 0
+        positions: [1, 2, 3, 4, 5, 6, 7, 8, 9], turn_number: 0
       }
-      
+
     end
     # Sends menu with play:select_position callback
     msg = {
       text: 'Please choose your position?',
-      attachments: self.board_last(user_id)
+      attachments: board_last(user_id)
     }
-  
     # Send message
-   
     API.send_response(url, msg)
   end
 
@@ -207,20 +194,22 @@ class Bot
           name: 'start',
           text: $plays[user_id][:positions][0].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][0],
+          value: $plays[user_id][:positions][0]
         },
         {
           name: 'start',
           text: $plays[user_id][:positions][1].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][1],
+          value: $plays[user_id][:positions][1]
         },
         {
           name: 'start',
           text: $plays[user_id][:positions][2].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][2],
-        }]},{
+          value: $plays[user_id][:positions][2]
+        }
+      ]
+    }, {
       color: '#FFA500',
       title: '',
       callback_id: 'play:select_position',
@@ -229,20 +218,22 @@ class Bot
           name: 'start',
           text: $plays[user_id][:positions][3].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][3],
+          value: $plays[user_id][:positions][3]
         },
         {
           name: 'start',
           text: $plays[user_id][:positions][4].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][4],
+          value: $plays[user_id][:positions][4]
         },
         {
           name: 'start',
           text: $plays[user_id][:positions][5].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][5],
-        }]},{
+          value: $plays[user_id][:positions][5]
+        }
+      ]
+    }, {
       color: '#FFA500',
       title: '',
       callback_id: 'play:select_position',
@@ -251,20 +242,21 @@ class Bot
           name: 'start',
           text: $plays[user_id][:positions][6].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][6],
+          value: $plays[user_id][:positions][6]
         },
         {
           name: 'start',
           text: $plays[user_id][:positions][7].to_s,
           type: 'button',
-          value: $plays[user_id][:positions][7],
+          value: $plays[user_id][:positions][7]
         },
         {
           name: 'start',
           text: $plays[user_id][:positions][8].to_s,
           type: 'button',
           value: $plays[user_id][:positions][8]
-        }]
+        }
+      ]
     }]
   end
 
@@ -272,20 +264,20 @@ class Bot
   def self.handle_direct_message(msg)
     user = msg['user']
     if $plays[user].nil?
-      self.intro(user)
+      intro(user)
     else
       $client.chat_postMessage(
-        channel: msg['channel'], 
+        channel: msg['channel'],
         text: 'Let\'s keep playing the last game'
       )
     end
   end
 
-  def self.update_board(request_data,chosen,symbol)
+  def self.update_board(request_data, chosen, symbol)
     user_id = request_data['user']['id']
-    #update chosen number 
-    $plays[user_id][:positions][chosen-1] = symbol 
-    self.increase_turn_number(user_id)
+    # update chosen number
+    $plays[user_id][:positions][chosen - 1] = symbol
+    increase_turn_number(user_id)
   end
 
   def self.increase_turn_number(user_id)
@@ -293,13 +285,16 @@ class Bot
   end
 
   def self.choose_position(user_id)
-    available_positions = self.check_positions(user_id)
-    return available_positions.uniq.select {|item| available_positions.count(item)==1}[0] if available_positions.class == Array
-    $plays[user_id][:positions].select {|item| item.class == Integer }.sample
+    available_positions = check_positions(user_id)
+    if available_positions.class == Array
+      return available_positions.uniq.select { |item| available_positions.count(item) == 1 }[0]
+    end
+
+    $plays[user_id][:positions].select { |item| item.class == Integer }.sample
   end
 
-  def self.check_winner_draw(user_id,url,msg)
-    winner = self.check_positions(user_id)
+  def self.check_winner_draw(user_id, url, msg)
+    winner = check_positions(user_id)
     turn = $plays[user_id][:turn_number]
     p turn
     p '============='
@@ -317,21 +312,23 @@ class Bot
             name: 'start',
             text: '   YES   ',
             type: 'button',
-            value: 'play:start',
+            value: 'play:start'
           }
-          
-          ]},{
-            color: '#FF0000',
-            title: '',
-            callback_id: 'play:finish',
-            actions: [
-              {
-                name: 'start',
-                text: '   NO   ',
-                type: 'button',
-                value: 'play:finish',
-              }
-              ]}]
+
+        ]
+      }, {
+        color: '#FF0000',
+        title: '',
+        callback_id: 'play:finish',
+        actions: [
+          {
+            name: 'start',
+            text: '   NO   ',
+            type: 'button',
+            value: 'play:finish'
+          }
+        ]
+      }]
 
       API.send_response(url, msg)
       $plays[user_id] = nil
@@ -348,21 +345,21 @@ class Bot
       [board[6], board[7], board[8]]
     ]
     # check the rows
-    return self.check_rows(grid) unless self.check_rows(grid).nil?
+    return check_rows(grid) unless check_rows(grid).nil?
 
     # check the columns
-    return self.check_columns(grid) unless self.check_columns(grid).nil?
+    return check_columns(grid) unless check_columns(grid).nil?
 
     # check the diagonals
-    return self.check_diagonals(grid) unless self.check_diagonals(grid).nil?
+    return check_diagonals(grid) unless check_diagonals(grid).nil?
 
     nil
   end
 
   def self.check_rows(grid)
-    grid.each { |row| return row.first if self.all_equal?(row) }
-    grid.each { |row| return row if (self.any_equal?(row) && row.any? {|item| item.class == Integer})}
-    
+    grid.each { |row| return row.first if all_equal?(row) }
+    grid.each { |row| return row if any_equal?(row) && row.any? { |item| item.class == Integer } }
+
     nil
   end
 
@@ -375,7 +372,7 @@ class Bot
   end
 
   def self.check_columns(grid)
-    self.check_rows(grid.transpose)
+    check_rows(grid.transpose)
   end
 
   def self.check_diagonals(grid)
@@ -383,6 +380,6 @@ class Bot
       [grid[0][0], grid[1][1], grid[2][2]],
       [grid[2][0], grid[1][1], grid[2][0]]
     ]
-    self.check_rows(diagonals)
+    check_rows(diagonals)
   end
 end
