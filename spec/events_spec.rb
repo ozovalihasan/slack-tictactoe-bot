1e33f3616b04af173b6a642758e8d1237b65fcd0
Hasan Ozovali <ozovalihasan@gmail.com>
2020-06-12 02:45:42 +0300

    Fix errors found by Rubocop

--- spec/events_spec.rb
+++ spec/events_spec.rb
@@ -3,9 +3,9 @@ require_relative '../lib/events'
 describe Events do
   describe '#message' do
     it 'detects any chosen event happened on Slack and starts the app' do
-    # Test is not possible
-    # because connecting to Slack is necessary to test it
-    # Any way for testing the method is not known
+      # Test is not possible
+      # because connecting to Slack is necessary to test it
+      # Any way for testing the method is not known
     end
   end
 end
